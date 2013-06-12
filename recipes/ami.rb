e05d6ffab5a00e0c89d43fa6eb454e5068b2ef13
Jake Davis <jake.davis5989@gmail.com>
2013-06-12 21:22:52 +0000

    Post-sanitize internal sync

---
+++ recipes/ami.rb
@@ -0,0 +1,33 @@
+# recipes/ami.rb
+# 
+# Author: Simple Finance <ops@simple.com>
+# License: Apache License, Version 2.0
+#
+# Copyright 2013 Simple Finance Technology Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Updates Jenkins instance for AMI building privs
+
+template '/etc/sudoers.d/ami' do
+  source 'sudoers.erb'
+  owner 'root'
+  group 'root'
+  mode '0440'
+  variables(
+    :users => [node[:jenkins][:node][:user]],
+    :commands => [
+      "/bin/mount /dev/xvd* #{node[:builder][:tmp_dir]}/*",
+      "/bin/umount -l #{node[:builder][:tmp_dir]}/*" ] )
+end
+
